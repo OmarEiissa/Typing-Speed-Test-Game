5e1c8c052093b8c5f2a82ed15f139a1c3fc4c4c6
OmarEiissa <eissa0383@gmail.com>
2024-09-23 02:07:26 +0300

    new project

---
+++ js/main.js
@@ -0,0 +1,137 @@
+// Array Of Words
+const words = [
+  "Hello",
+  "programming",
+  "Code",
+  "JavaScript",
+  "Town",
+  "Country",
+  "Testing",
+  "youtube",
+  "linkedin",
+  "Twitter",
+  "Github",
+  "Leetode",
+  "Internet",
+  "Python",
+  "Scala",
+  "Destructuring",
+  "Paradigm",
+  "Styling",
+  "Cascade",
+  "Documentation",
+  "Coding",
+  "Funny",
+  "Working",
+  "Dependencies",
+  "Task",
+  "Runner",
+  "Roles",
+  "Test",
+  "Rust",
+  "playing",
+];
+
+// Setting Levels
+const lvlS = {
+  Easy: 5,
+  Normal: 3,
+  Hard: 2,
+};
+
+// Default Level
+let defaultLevelName = "Normal"; // Change Level From Here
+let defaultLevelSeconds = lvlS[defaultLevelName];
+
+// Catch Selectors
+let startButton = document.querySelector(".start");
+let lvlNameSpan = document.querySelector(".message .lvl");
+let secondsSpan = document.querySelector(".message .seconds");
+let theWord = document.querySelector(".the-word");
+let upcomingWords = document.querySelector(".upcoming-word");
+let input = document.querySelector(".input");
+let timeLeftSpan = document.querySelector(".time span");
+let scoreGot = document.querySelector(".score .got");
+let scoreTotal = document.querySelector(".score .total");
+let finishMessage = document.querySelector(".finish");
+
+// Setting Level Name + Seconds + Score
+lvlNameSpan.innerHTML = defaultLevelName;
+secondsSpan.innerHTML = defaultLevelSeconds;
+timeLeftSpan.innerHTML = defaultLevelSeconds;
+scoreTotal.innerHTML = words.length;
+
+// Disable Paste Event
+input.onpaste = function () {
+  return false;
+};
+
+// Start Game
+startButton.onclick = function () {
+  this.remove();
+  input.focus();
+  // Generate Word Function
+  genWords();
+};
+
+function genWords() {
+  // Get Random Word From Array
+  let randomWord = words[Math.floor(Math.random() * words.length)];
+  // Get word index
+  let wordIndex = words.indexOf(randomWord);
+  // Remove word From Array
+  words.splice(wordIndex, 1);
+  // Show The Random Word
+  theWord.innerHTML = randomWord;
+  // Empty Upcoming Words
+  upcomingWords.innerHTML = "";
+  // Generate Words
+  for (let i = 0; i < words.length; i++) {
+    // Create Div Element
+    let div = document.createElement("div");
+    let txt = document.createTextNode(words[i]);
+    div.appendChild(txt);
+    upcomingWords.appendChild(div);
+  }
+  // Call Start Play Function
+  startPlay();
+}
+
+function startPlay() {
+  timeLeftSpan.innerHTML = defaultLevelSeconds;
+  let start = setInterval(() => {
+    timeLeftSpan.innerHTML--;
+    if (timeLeftSpan.innerHTML === "0") {
+      // Stop Timer
+      clearInterval(start);
+      // Compare Words
+      if (
+        theWord.innerHTML.toLocaleLowerCase() ===
+        input.value.toLocaleLowerCase()
+      ) {
+        // Empty Input Field
+        input.value = "";
+        // Increase Score
+        scoreGot.innerHTML++;
+        if (words.length > 0) {
+          // Call Generate Word Function
+          genWords();
+        } else {
+          let span = document.createElement("span");
+          span.className = "good";
+          let spanText = document.createTextNode("Congrats");
+          span.appendChild(spanText);
+          finishMessage.appendChild(span);
+          // Remove Upcoming Word Box
+          upcomingWords.remove()
+        }
+      } else {
+        let span = document.createElement("span");
+        span.className = "bad";
+        let spanText = document.createTextNode("Game Over");
+        span.appendChild(spanText);
+        finishMessage.appendChild(span);
+      }
+    }
+  }, 1000);
+}
